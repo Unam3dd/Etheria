8115dd90ef092c061f173611ae080f376cc51654
Unam3dd <samtalespro@gmail.com>
2024-02-06 12:53:57 +0100

    update documentation

--- inc/bytebreacher.h
+++ inc/bytebreacher.h
@@ -1,6 +1,20 @@
+/*
+ * @file bytebreacher.h
+ * @brief Main includes of the program
+ * @author Unam3dd aka sam0verfl0w
+ */
+
+/*
+ * @def BYTEBREACHER_H includes guards
+ */
 #ifndef BYTEBREACHER_H
 #define BYTEBREACHER_H
 
+/*
+ * @def __BYTEBREACHER_VERSION__ define a version of the project
+ * @brief define ByteBreacher Version
+ * @note if not defined the value is undefined
+ */
 #ifndef __BYTEBREACHER_VERSION__
 #define __BYTEBREACHER_VERSION__ "undefined"
 #endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
-#include "bytebreacher.h"
+/*! @file main.c
+ *  @brief  Main of bytebreacher
+ *  @note no note
+ *  @bug no bugs known
+ *  @version 0.0.1
+ *  @date 06/02/2024 12:32:37
+ *  @author Unam3dd (sam0verfl0w)
+ *  @copyright Copyright (C) 2024 Sam0verfl0w
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
-    printf("ByteBreacher Version : %s\n", __BYTEBREACHER_VERSION__);
     return (0);
 }
